efff8dfa871913235a24287fad7ca4971935c1b2
Callum Kloos <callumkloos@gmail.com>
2023-11-29 11:07:22 +0000

    tailwind config update

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,9 +1,23 @@
 import type { Config } from "tailwindcss";
+import { mauve, ruby, cyan, mint, blackA, whiteA } from "@radix-ui/colors";
 
 export default {
 	content: ["./app/**/*.{js,jsx,ts,tsx}"],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				...mauve,
+				...ruby,
+				...cyan,
+				...mint,
+				...blackA,
+				...whiteA,
+			},
+		},
 	},
-	plugins: [],
+	plugins: [
+		require("tailwindcss-animate"),
+		require("tailwindcss-react-aria-components"),
+		require("@tailwindcss/typography"),
+	],
 } satisfies Config;
